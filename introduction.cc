41806ef97955c8cf97e4cfcd932f0106e3ea0b13
JCS28 <70351809+JCS28@users.noreply.github.com>
2020-09-04 19:03:06 -0500

    Update introduction.cc

--- introduction.cc
+++ introduction.cc
@@ -1,6 +1,6 @@
 /* 	Justin Smith
 	z1842645
-	CSCI 340 - 2
+	CSCI 340-2
 	
 	I certify that this is my own work and where appropriate an extension 
 	of the starter code provided for the assignment.
